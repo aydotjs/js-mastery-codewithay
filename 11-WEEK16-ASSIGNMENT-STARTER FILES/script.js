b2ac51115b1aa73643c8c65f0d1524bc3b84f3fb
aydotjs <ayobami66@gmail.com>
2022-10-09 19:32:40 +0100

    ssignment added

---
+++ 11-WEEK16-ASSIGNMENT-STARTER FILES/script.js
@@ -0,0 +1,11 @@
+//Hint
+//Critically Examine the mark up and the CSS
+//Select the left and right buttons
+//Select element with id of imgs
+//select all the images
+//crate a function to change image on button click
+//you may need to learn about about the setInterval method
+//you will need to transform the images offscreen to implement sliding effect
+//Goodluck😎 
+
+"use strict";
